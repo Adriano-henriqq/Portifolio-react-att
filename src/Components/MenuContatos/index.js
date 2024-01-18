d4c08fd9066cdfafe2d184246f5cd89f377cfc1e
Adriano Henrique <adriano_uchiha@hotmail.com>
2024-01-18 11:52:43 -0300

    adicionei url para os contatos

--- src/Components/MenuContatos/index.js
+++ src/Components/MenuContatos/index.js
@@ -8,10 +8,10 @@ export default function MenuContatos(){
     return(
         <ul className={styles.lista}>
             <li>
-                <a href='#ssd'><GitHubIcon fontSize='large'  /></a>
+                <a href='https://github.com/Adriano-henriqq'><GitHubIcon fontSize='large'  /></a>
             </li>
             <li>
-                <a href='https'><LinkedInIcon fontSize='large' /></a>
+                <a href='https://www.linkedin.com/in/adriano-henrique-medeiros-85b95b180/'><LinkedInIcon fontSize='large' /></a>
             </li>
             
         </ul>
